c1cae7aa618ef5d199750eb3174cb8be703f92de
wuwudy <2806067876@qq.com>
2024-11-17 10:35:37 +0800

    0.4 git 测试

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,10 @@ export default defineConfig({
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp'
     }
-  }
+  },
+  // resolve: {
+  //   alias: {
+  //     '@ffmpeg-types': path.resolve(__dirname, 'node_modules/@ffmpeg/ffmpeg/dist/esm/types'),
+  //   },
+  // },
 })
